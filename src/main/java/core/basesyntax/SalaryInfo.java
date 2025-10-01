250660bdfec6e303a54e0d8e5b5ad7288752b536
Vovald <vtol2003@gmail.com>
2025-10-01 20:17:55 +0300

    add task solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,33 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+
+        StringBuilder report = new StringBuilder("Report for period " + dateFrom + " - " + dateTo);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate startDate = LocalDate.parse(dateFrom, formatter);
+        LocalDate endDate = LocalDate.parse(dateTo, formatter);
+
+        for (String name : names) {
+            int totalSalary = 0;
+            for (String record : data) {
+                String[] parts = record.split(" ");
+                String recordName = parts[1];
+
+                if (name.equals(recordName)) {
+                    LocalDate recordDate = LocalDate.parse(parts[0], formatter);
+                    if (!recordDate.isBefore(startDate) && !recordDate.isAfter(endDate)) {
+                        int hours = Integer.parseInt(parts[2]);
+                        int rate = Integer.parseInt(parts[3]);
+                        totalSalary += hours * rate;
+                    }
+                }
+            }
+            report.append(System.lineSeparator()).append(name).append(" - ").append(totalSalary);
+        }
+        return report.toString();
     }
 }
